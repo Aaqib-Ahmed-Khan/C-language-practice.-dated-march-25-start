e11d20d3491eb75832cfecfc371a7bd7b38b6e80
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-05-28 15:25:11 +0500

    funstions

---
+++ function1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void karachi(){
+    printf("you are in karachi");
+    return;
+}
+  void lahore(){
+    printf("you are in karachi");
+    return;
+  }
+    void islamabad(){
+    printf("you are in karachi");
+    return;
+    }
+    void peshawer(){
+    printf("you are in karachi");
+    return;
+    }
+int main(){
+    karachi();
+
+}
