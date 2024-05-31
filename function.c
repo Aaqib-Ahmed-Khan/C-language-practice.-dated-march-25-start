f96244435a6d871df1f2c0ce56a16f4d687098ae
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-05-31 21:46:43 +0500

    add2

--- function.c
+++ function.c
@@ -1,6 +1,7 @@
 Function is a block of code which has some name for identfication.
 
 All functions name must be unique.
+no keyword is function like return while.
 #include<stdio.h>
 int main(){
     void fun();
