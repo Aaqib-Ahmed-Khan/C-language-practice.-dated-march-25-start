e6a7b94a641e3c490d1b13bdbc07609bd753d3b9
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-02 23:47:56 +0500

    2

--- function2.c
+++ function2.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
-void caclulate()
-int main()
-{
-printf(%d)
+// void caclulate()
+// int main()
+// {
+// printf(%d)
+// }
+void hello();
+
+int main(){
+     hello();
+    return 0;
+}
+void hello  (){
+    printf("hello"); 
 }
