e822d27f094c3ee327d965a2d55312cdeb0d8e84
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-03 00:07:47 +0500

    new programline 42

--- function2.c
+++ function2.c
@@ -20,22 +20,36 @@
 // void goodbye  (){
 //     printf("goodbye"); 
 // }
-void cricket();
-void football();
+// void cricket();
+// void football();
+// int main(){
+// printf("enter c for cricket or f for football\n");
+// char ch;
+// scanf("%c",&ch);
+// if(ch=='c'){
+//     cricket();
+// }else{
+//     football();
+// }
+// return 0;
+// }
+// void cricket(){
+//     printf("cricket\n");
+// }
+// void football(){
+// printf("football\n");
+// }
+void biryani();
+void tikka();
 int main(){
-printf("enter c for cricket or f for football\n");
-char ch;
-scanf("%c",&ch);
-if(ch=='c'){
-    cricket();
-}else{
-    football();
+    printf("choose any one\n");
+    scanf("%c",&ch);
+    if(ch=='b'){
+        biryani();
+
+    }
+    else{
+        tikka();
 }
-return 0;
-}
-void cricket(){
-    printf("cricket\n");
-}
-void football(){
-printf("football\n");
-}
+void biryani()
+printf 
